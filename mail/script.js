df0696e6c3a5a0b01c1a3af885ab20d456fb2580
scollo26 <giovanniscollo26@gmail.com>
2021-11-24 14:31:19 +0100

    esercizio email-con input

--- mail/script.js
+++ mail/script.js
@@ -1,40 +1,44 @@
 // const email = prompt('inserisci email');
 
 
-const listaEmail =[ "giovanni", "luca", "davide" ];
-console.log(listaEmail);
-const cond = false;
-for(let i=0; i < listaEmail.length; i++){
+// const listaEmail =[ "giovanni", "luca", "davide" ];
+// console.log(listaEmail);
+// const cond = false;
+// for(let i=0; i < listaEmail.length; i++){
     
-    if( email == listaEmail[i]){
-        cond = true;
-    }
-}
-if(cond == true){
-    document.writeln('sei nella lista')
-}else{
-    document.writeln('non sei nella lista')
-    console.log(' non sei nella lista'); 
-}
+//     if( email == listaEmail[i]){
+//         cond = true;
+//     }
+// }
+// if(cond == true){
+//     document.writeln('sei nella lista')
+// }else{
+//     document.writeln('non sei nella lista')
+//     console.log(' non sei nella lista'); 
+// }
 
 
 
 
 const container = document.querySelector('.container');
-const input = document.querySelector('email');
+const input = document.querySelector('input');
 const button = document.querySelector('button');
 const listaEmail =[ "giovanni", "luca", "davide" ];
+let cond = false;
 
-
-for(let i=0; i < listaEmail.length; i++){
+button.addEventListener('click', function(){
+    for(let i=0; i < listaEmail.length; i++){
     
-    if( input.value == listaEmail[i]){
-        cond = true;
+        if(listaEmail[i] == input.value){
+            cond = true;
+        }
     }
-}
-if(cond == true){
-    document.writeln('sei nella lista')
-}else{
-    document.writeln('non sei nella lista')
-    console.log(' non sei nella lista'); 
-}
+    if(cond == true){
+        container.innerHTML = "sei nella lista";
+    }else{
+        container.innerHTML = "non sei nella lista";
+        console.log(' non sei nella lista');
+         
+    }
+
+});
